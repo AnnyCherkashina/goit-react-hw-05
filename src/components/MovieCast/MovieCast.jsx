62c353224d54f59c8c2c79be9338c4c3c7e92c84
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2024-06-22 14:39:32 +0300

    update MovieCast< delete suspense

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Loader from "../Loader/Loader";
@@ -9,33 +9,43 @@ import s from "./MovieCast.module.css";
 const MovieCast = () => {
     const { movieId } = useParams();
     const [cast, setCast] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
+        setLoading(true);
         fetchMovieCast(movieId)
             .then((data) => setCast(data))
-            .catch((error) => console.log(error));
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
     }, [movieId]);
 
+    if (loading) {
+        return <Loader />;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
     return (
-        <Suspense fallback={<Loader />}>
-            <div className={s.cast}>
-                {cast && (
-                    <ul className={s.list}>
-                        {cast.cast.map((actor) => (
-                            <li key={actor.id} className={s.item}>
-                                <img
-                                    className={s.poster}
-                                    src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-                                    alt={actor.name}
-                                />
-                                <p className={s.name}>{actor.name}</p>
-                                <p className={s.character}>Character: {actor.character}</p>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-        </Suspense>
+        <div className={s.cast}>
+            {cast && (
+                <ul className={s.list}>
+                    {cast.cast.map((actor) => (
+                        <li key={actor.id} className={s.item}>
+                            <img
+                                className={s.poster}
+                                src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                                alt={actor.name}
+                            />
+                            <p className={s.name}>{actor.name}</p>
+                            <p className={s.character}>Character: {actor.character}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 };
 
